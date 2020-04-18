e38f4770733594b3a9889d57825259fe6dab4525
NafiAsib <nafi.asib@gmail.com>
2020-04-18 11:45:21 +0600

    cf 1337A

---
+++ codeforces/1337A.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#define watch(x) cerr <<"at line "<< __LINE__ <<", " << #x << " = " << x << "\n"
+using namespace std;
+
+int main()
+{
+    int tc;
+    cin >> tc;
+    while(tc--) {
+        int a, b, c, d;
+        cin >> a >> b >> c >> d;
+        cout<<a<< " " << c << " " << c << "\n";
+    }
+
+    return 0;
+}
