c2d7a10b6fe7446d1db7ebde2de832e78d379b9e
NafiAsib <nafi.asib@gmail.com>
2020-06-19 14:28:42 +0600

    CF 1366A

---
+++ codeforces/1366A.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define watch(x) cout<<(#x)<<" is "<<(x)<<endl
+typedef pair<int, int> pii;
+typedef long long int64;
+const double eps = 1e-9;
+const int inf = 0x3f3f3f3f;
+const long long linf = 0x3f3f3f3f3f3f3f3fLL;
+
+int main()
+{
+    #ifndef ONLINE_JUDGE
+	    freopen("../io/in.txt", "r", stdin);
+	    //freopen("../io/out.txt", "w", stdout);
+    #endif // ONLINE_JUDGE
+    int tc;
+    cin >> tc;
+    while(tc--) {
+        int a, b;
+        cin >> a >> b;
+        cout << min({a, b, (a+b)/3}) << "\n";
+    }
+    return 0;
+}
+
